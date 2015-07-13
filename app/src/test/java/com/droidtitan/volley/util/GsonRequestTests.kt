bff78f8c7be8b9df14ad4d5061639733eaae96bc
marco <marsucsb@gmail.com>
2015-07-13 00:26:33 -0700

    Add GsonRequest tests.

---
+++ app/src/test/java/com/droidtitan/volley/util/GsonRequestTests.kt
@@ -0,0 +1,45 @@
+package com.droidtitan.volley.util
+
+import com.android.volley.NetworkError
+import com.android.volley.Response.ErrorListener
+import com.android.volley.Response.Listener
+import com.droidtitan.volley.App
+import com.droidtitan.volley.BuildConfig
+import com.droidtitan.volley.model.air.AirQualityResponse
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.robolectric.RobolectricGradleTestRunner
+import org.robolectric.annotation.Config
+import kotlin.properties.Delegates
+import kotlin.test.assertEquals
+
+@Config(sdk = intArrayOf(21), application = App::class, constants = BuildConfig::class)
+@RunWith(RobolectricGradleTestRunner::class)
+public class GsonRequestTests {
+
+    var request: GsonRequest<AirQualityResponse> by Delegates.notNull()
+    var errorListener: ErrorListener by Delegates.notNull()
+
+    @Before fun setUp() {
+        val url = "http://responseUrl.com"
+        val clazz = javaClass<AirQualityResponse>()
+
+        val listener = mock(javaClass<Listener<AirQualityResponse>>())
+        errorListener = mock(javaClass<ErrorListener>())
+        request = GsonRequest(url, clazz, null, listener, errorListener)
+    }
+
+    @Test fun testErrorListenerResponseCalled() {
+        val error = NetworkError()
+        request.deliverError(error)
+        verify(errorListener, times(1)).onErrorResponse(error)
+    }
+
+    @Test fun testAcceptHeaderisAdded() {
+        assertEquals("application/json", request.getHeaders().get("Accept"))
+    }
+}
