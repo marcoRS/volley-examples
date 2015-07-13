f813d1bf0781cab5301c8b0c165f1ce903510b53
marco <marsucsb@gmail.com>
2015-07-12 22:19:39 -0700

    Add Volley Extension Test.

---
+++ app/src/test/java/com/droidtitan/volley/util/VolleyExtensionsTests.kt
@@ -0,0 +1,23 @@
+package com.droidtitan.volley.util
+
+import android.content.res.Resources
+import com.android.volley.NetworkError
+import com.android.volley.NetworkResponse
+import org.junit.Test
+import org.mockito.Matchers.anyInt
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import kotlin.test.assertEquals
+
+public class VolleyExtensionsTests {
+
+    @Test fun testNetworkErrorMessageReturned() {
+        val expected = "No connection. Please try again."
+        val error = NetworkError(NetworkResponse(404, null, null, true))
+
+        val res = Mockito.mock(javaClass<Resources>())
+        `when`(res.getString(anyInt())).thenReturn(expected)
+
+        assertEquals(expected, error.toString(res))
+    }
+}
